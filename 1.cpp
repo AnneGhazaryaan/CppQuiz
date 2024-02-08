e9eb87bc3cbd0948df218244a794881ab4231b86
Anahit Ghazaryan <146667359+AnneGhazaryaan@users.noreply.github.com>
2024-02-08 20:50:49 +0400

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+struct A {
+    A() { std::cout << "A"; }
+    ~A() { std::cout << "a"; }
+};
+
+int main() {
+    std::cout << "main";
+    return sizeof new A;
+}
+
+//main
