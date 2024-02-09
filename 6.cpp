9fcd37a747a0dd7dcaffab8a55bf1a5a6ebb80e8
Anahit Ghazaryan <146667359+AnneGhazaryaan@users.noreply.github.com>
2024-02-09 19:32:47 +0400

    Create 6.cpp

---
+++ 6.cpp
@@ -0,0 +1,19 @@
+According to the C++17 standard, what is the output of this program?
+
+    
+#include <iostream>
+using namespace std;
+
+int foo() {
+  cout << 1;
+  return 1;
+}
+
+void bar(int i = foo()) {}
+
+int main() {
+  bar();
+  bar();
+}
+
+//11
